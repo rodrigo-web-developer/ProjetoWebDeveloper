f3627e3b8a65b9add4764828dc34951561482235
RodrigoRodriguesX10 <x10rodrigo@gmail.com>
2018-03-08 21:27:31 -0300

    adicionado controllers

---
+++ PrimeiraAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpPost("somar")]
+        public double Somar([FromBody] Dictionary<string, string> valores)
+        {
+            var numero1 = double.Parse(valores["numero1"]);
+            var numero2 = double.Parse(valores["numero2"]);
+            return numero1 + numero2;
+        }
+    }
+}
