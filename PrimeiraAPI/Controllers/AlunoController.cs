b568c525173ac39fc4b88635afabdb571c66dc42
RodrigoRodriguesX10 <x10rodrigo@gmail.com>
2018-03-23 19:12:13 -0300

    Aula 4 CSharp - Criado AlunoController com Listar e Criar

---
+++ PrimeiraAPI/Controllers/AlunoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPI.Models;
+using System.Collections.Generic;
+
+namespace PrimeiraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlunoController : Controller
+    {
+        public static List<Aluno> Alunos = new List<Aluno>();
+
+        [HttpGet("")]
+        public List<Aluno> Listar()
+        {
+            return Alunos;
+        }
+        [HttpPost("")]
+        public Aluno Criar([FromBody] Aluno a)
+        {
+            Alunos.Add(a);
+            return a;
+        }
+    }
+}
---
+++ PrimeiraAPI/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeiraAPI.Models
+{
+    public class Aluno
+    {
+        private int idade; // campo
+        public string Ra { get; set; } // propriedade
+        public string Nome { get; set; } // propriedade
+
+        public DateTime DataNascimento { get; set; } // propriedade
+        public int Idade
+        { // propriedade com apenas get
+            get
+            {
+                idade = DateTime.Today.Year - DataNascimento.Year;
+                var x = DataNascimento.AddYears(idade);
+                if (DateTime.Today < x)
+                    idade--;
+                return idade;
+            }
+        }
+    }
+}
